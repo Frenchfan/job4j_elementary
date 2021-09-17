01a2bdd604a37bcef22ce73cfd24dd064fb6a321
Frenchfan <sumkin.gpb@gmail.com>
2021-09-18 02:27:37 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Peter Sumkin");
+        System.out.println("01.12.1984");
+    }
+}
