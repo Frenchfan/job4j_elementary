266cee11ee88910cd797b67c3a0ac1279723799c
Frenchfan <sumkin.gpb@gmail.com>
2021-11-03 23:59:52 +0300

    5. Конвертер валюты [#41583 #219925]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -9,5 +9,9 @@ public class Point {
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
+        double result2 = Point.distance(4, 3, 7, 7);
+        System.out.println("result (4, 3) to (7, 7) " + result2);
+        double result3 = Point.distance(2, 3, 43, 26);
+        System.out.println("result (2, 3) to (43, 26) " + result3);
     }
 }
