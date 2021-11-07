cf8bd69feba08fc19f25998f990b7ba42b360cf5
Frenchfan <sumkin.gpb@gmail.com>
2021-11-07 23:55:05 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Kartashova Elena";
+        names[2] = "Esipov Alexey";
+        names[3] = "Parshin Sergey";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
