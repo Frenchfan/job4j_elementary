3709df7a516872ec694bf84605130521ae3545bd
Frenchfan <sumkin.gpb@gmail.com>
2021-11-09 01:47:46 +0300

    --

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -15,17 +15,4 @@ public class MatrixCheckTest {
         boolean result = MatrixCheck.monoHorizontal(input, row);
         Assert.assertTrue(result);
     }
-
-    @Test
-    public void whenHasMonoVertical() {
-        char[][] input = {
-                {' ', ' ', 'X'},
-                {' ', ' ', 'X'},
-                {' ', ' ', 'X'},
-        };
-        int column = 2;
-        boolean result = MatrixCheck.monoVertical(input, column);
-        Assert.assertTrue(result);
-    }
-
 }
